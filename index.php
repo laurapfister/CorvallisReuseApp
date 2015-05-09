3fadae7bee1467df923e63f010092c794f239877
pfisterl <pfisterl@onid.oregonstate.edu>
2015-05-09 09:01:21 -0700

    index.php added resuse GETS and first post

--- index.php
+++ index.php
@@ -161,6 +161,113 @@
 		$mysqli->close();
 
 	});
+
+	$app->get('/reuse', function(){
+		$mysqli = new mysqli("mysql.eecs.oregonstate.edu", "cs419-g4", "RNjFRsBYJK5DVF8d", "cs419-g4");
+		$result = $mysqli->query('SELECT reuseName, reuseAddress, reuseCity, reuseState, reuseZip, reuseWeb FROM reuse_businesses');
+		
+		while($row = $result->fetch_array(MYSQL_ASSOC)){
+			$myArray[] = $row;
+		}
+		echo json_encode($myArray);
+		$result->close();
+		$mysqli->close();
+
+	});
+
+	$app->get('/reuse/:id', function($id){
+		$mysqli = new mysqli("mysql.eecs.oregonstate.edu", "cs419-g4", "RNjFRsBYJK5DVF8d", "cs419-g4");
+		$stmt = $mysqli->prepare("SELECT reuseName, reuseAddress, reuseCity, reuseState, reuseZip, reuseWeb FROM reuse_businesses WHERE reuseId = ?");
+		$stmt->bind_param("i", $id);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		while($row = $result->fetch_array(MYSQL_ASSOC)){
+			$myArray[] = $row;
+		}
+		echo json_encode($myArray);
+		$result->close();
+		$mysqli->close();
+	});
+
+	$app->get('/reuseItems', function(){
+		$mysqli = new mysqli("mysql.eecs.oregonstate.edu", "cs419-g4", "RNjFRsBYJK5DVF8d", "cs419-g4");
+		$result = $mysqli->query('SELECT ri.itemName, rc.categoryName FROM reuse_items AS ri INNER JOIN reuse_categories as rc ON rc.categoryId = ri.categoryId');
+		
+		while($row = $result->fetch_array(MYSQL_ASSOC)){
+			$myArray[] = $row;
+		}
+		echo json_encode($myArray);
+		$result->close();
+		$mysqli->close();
+	});
+
+	$app->get('/reuseCategory', function() {
+		$mysqli = new mysqli("mysql.eecs.oregonstate.edu", "cs419-g4", "RNjFRsBYJK5DVF8d", "cs419-g4");
+		$result = $mysqli->query('SELECT categoryName FROM reuse_categories');
+		
+		while($row = $result->fetch_array(MYSQL_ASSOC)){
+			$myArray[] = $row;
+		}
+		echo json_encode($myArray);
+		$result->close();
+		$mysqli->close();
+	});
+
+	$app->get('/reuseItems/:category', function($category){
+		$mysqli = new mysqli("mysql.eecs.oregonstate.edu", "cs419-g4", "RNjFRsBYJK5DVF8d", "cs419-g4");
+		$stmt = $mysqli->prepare("SELECT ri.itemName FROM reuse_items AS ri INNER JOIN reuse_categories as rc ON rc.categoryId = ri.categoryId WHERE rc.categoryName = ?");
+		$stmt->bind_param("s", $category);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		while($row = $result->fetch_array(MYSQL_ASSOC)){
+			$myArray[] = $row;
+		}
+		echo json_encode($myArray);
+		$result->close();
+		$mysqli->close();
+
+	});
+
+	$app->get('/reuseBus/:category', function($category){
+		$mysqli = new mysqli("mysql.eecs.oregonstate.edu", "cs419-g4", "RNjFRsBYJK5DVF8d", "cs419-g4");
+		$stmt = $mysqli->prepare("SELECT reuseName, reuseAddress, reuseCity, reuseState, reuseZip, reuseWeb FROM reuse_businesses AS rb INNER JOIN reuse_bus_categories AS rbc ON rbc.bid = rb.reuseId INNER JOIN reuse_categories as rc ON rc.categoryID = rbc.cid WHERE rc.categoryName = ?");
+		$stmt->bind_param("s", $category);
+		$stmt->execute();
+		$result = $stmt->get_result();
+		while($row = $result->fetch_array(MYSQL_ASSOC)){
+			$myArray[] = $row;
+		}
+		echo json_encode($myArray);
+		$result->close();
+		$mysqli->close();
+	});
+
+	$app->post('/reuse', function() use($app){
+		$mysqli = new mysqli("mysql.eecs.oregonstate.edu", "cs419-g4", "RNjFRsBYJK5DVF8d", "cs419-g4");
+		$request = $app->request();
+		$body = $request->getBody();
+		$params = json_decode($body);
+
+		$name = (string)$params->reuseName;
+		$address = (string)$params->reuseAddress;
+		$state = (string)$params->state;
+		$city = (string)$params->city;
+		$zip = (string)$params->zip;
+		$phone = (string)$params->phone;
+		$web = (string)$params->web;
+		$hours = (string)$params->hours;
+		$long = (double)$params->longitude;
+		$lat = (double)$params->latitude;
+
+		$stmt = $mysqli->prepare("INSERT INTO reuse_businesses(reuseName, reuseAddress, reuseCity, reuseState, reuseZip, reusePhone, reuseWeb,reuseHours, reuseLongitude, reuseLatitude) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("ssssssssdd", $name, $address, $city, $state, $zip, $phone, $web, $hours, $long, $lat);
+		$stmt->execute();
+		$mysqli->close();
+		
+	});
+
+	
+
 	
 	$app->run();
 ?>
